109b78fb1d2b67d968c5f87c89979b5170ee662a
Pyrix25633 <biral.mattia@gmail.com>
2022-11-10 17:43:43 +0100

    0.1.0 - Modifications to the gui setup

--- src/main/java/net/pyrix25633/ncis/Game.java
+++ src/main/java/net/pyrix25633/ncis/Game.java
@@ -4,13 +4,12 @@ import net.pyrix25633.ncis.gui.GameWindow;
 
 public class Game {
     private final GameWindow window;
-    private final String version;
+    public static final String version = "0.1.0";
 
     /**
      * Constructor
      */
     public Game() {
-        version = "0.1.0";
-        window = new GameWindow(version);
+        window = new GameWindow();
     }
 }
--- src/main/java/net/pyrix25633/ncis/gui/GameWindow.java
+++ src/main/java/net/pyrix25633/ncis/gui/GameWindow.java
@@ -1,25 +1,25 @@
 package net.pyrix25633.ncis.gui;
 
+import net.pyrix25633.ncis.Game;
+
 import javax.swing.*;
 
-public class GameWindow {
-    private final JFrame frame;
+public class GameWindow extends JFrame {
     private final GamePanel panel;
 
     /**
      * Constructor
-     * @param version The game version, String
      */
-    public GameWindow(String version) {
-        frame = new JFrame();
-        frame.setSize(600, 400);
-        frame.setTitle("Naval Criminal Investigative Service: The Game " + version);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+    public GameWindow() {
+        super();
+        this.setSize(600, 400);
+        this.setTitle("Naval Criminal Investigative Service: The Game " + Game.version);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
 
         panel = new GamePanel();
-        frame.add(panel);
+        this.add(panel);
 
-        frame.setVisible(true);
+        this.setVisible(true);
     }
 }
